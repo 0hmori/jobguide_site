a488b2cddd4d08c2a3b209d9cf43c108fa736beb
0hmori <mehowomori.fiofio.cute.38@gmail.com>
2023-11-06 21:36:20 +0900

    記事の登録など一通り作成済み、ロールカラムの追加のためマイグレーションファイル作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,9 @@ class PostController extends Controller
 
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::with('user')->latest()->paginate(4);
+
+        return view('posts.index', compact('posts'));
     }
 
     public function create()
@@ -27,7 +29,7 @@ class PostController extends Controller
         $post->user_id = $request->user()->id;
 
         $file = $request->file('image');
-        $post->image = date('YmdHis') . '_' . $file->getClientOriginalName();
+        $post->image = self::createFileName($file);
 
         // トランザクション開始
         DB::beginTransaction();
@@ -71,11 +73,83 @@ class PostController extends Controller
     public function update(UpdatePostRequest $request, string $id)
 
     {
-        //
+        $post = Post::find($id);
+
+        if ($request->user()->cannot('update', $post)) {
+            return redirect()->route('posts.show', $post)
+                ->withErrors('自分の記事以外は更新できません');
+        }
+
+        $file = $request->file('image');
+        if ($file) {
+            $delete_file_path = $post->image_path;
+            $post->image = self::createFileName($file);
+        }
+        $post->fill($request->all());
+
+        // トランザクション開始
+        DB::beginTransaction();
+        try {
+            // 更新
+            $post->save();
+
+            if ($file) {
+                // 画像アップロード
+                if (!Storage::putFileAs('images/posts', $file, $post->image)) {
+                    // 例外を投げてロールバックさせる
+                    throw new \Exception('画像ファイルの保存に失敗しました。');
+                }
+
+                // 画像削除
+                if (!Storage::delete($delete_file_path)) {
+                    //アップロードした画像を削除する
+                    Storage::delete($post->image_path);
+                    //例外を投げてロールバックさせる
+                    throw new \Exception('画像ファイルの削除に失敗しました。');
+                }
+            }
+
+            // トランザクション終了(成功)
+            DB::commit();
+        } catch (\Exception $e) {
+            // トランザクション終了(失敗)
+            DB::rollback();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('posts.show', $post)
+            ->with('notice', '登録内容を更新しました');
     }
 
     public function destroy(string $id)
     {
-        //
+        $post = Post::find($id);
+
+        // トランザクション開始
+        DB::beginTransaction();
+        try {
+            $post->delete();
+
+            // 画像削除
+            if (!Storage::delete($post->image_path)) {
+                // 例外を投げてロールバックさせる
+                throw new \Exception('画像ファイルの削除に失敗しました。');
+            }
+
+            // トランザクション終了(成功)
+            DB::commit();
+        } catch (\Exception $e) {
+            // トランザクション終了(失敗)
+            DB::rollback();
+            return back()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('posts.index')
+            ->with('notice', '記事を削除しました');
+    }
+
+    private static function createFileName($file)
+    {
+        return date('YmdHis') . '_' . $file->getClientOriginalName();
     }
 }
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,39 +1,20 @@
 <x-app-layout>
     <div class="container lg:w-1/2 md:w-4/5 w-11/12 mx-auto mt-8 px-8 bg-white shadow-md">
-        <h2 class="text-center text-lg font-bold pt-6 tracking-widest">出品編集</h2>
+        <h2 class="text-center text-lg font-bold pt-6 tracking-widest">登録内容編集</h2>
 
-        @if ($errors->any())
-            <div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 my-2" role="alert">
-                <p>
-                    <b>{{ count($errors) }}件のエラーがあります。</b>
-                </p>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        <x-validation-errors :errors="$errors" />
 
-        <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data"
+        <form action="{{ route('posts.update', $post) }}" method="POST" enctype="multipart/form-data"
             class="rounded pt-3 pb-8 mb-4">
             @csrf
             @method('PUT')
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm mb-2" for="category">
-                    カテゴリ
-                </label>
-                <input type="text" name="category"
-                    class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full py-2 px-3"
-                    required value="{{ old('category', $post->category) }}">
-            </div>
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm mb-2" for="title">
                     サービスタイトル
                 </label>
                 <input type="text" name="title"
                     class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full py-2 px-3"
-                    required placeholder="例) 除草作業を行います" value="{{ old('title',$post->title) }}">
+                    required placeholder="タイトル" value="{{ old('title', $post->title) }}">
             </div>
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm mb-2" for="body">
@@ -41,31 +22,7 @@
                 </label>
                 <textarea name="body" rows="10"
                     class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full py-2 px-3"
-                    required placeholder="サービスの内容、ユーザーのメリット、アピールしたい実績など具体的に書いてください">{{ old('body', $post->body) }}</textarea>
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm mb-2" for="price">
-                    サービス価格
-                </label>
-                <input type="integer" name="price"
-                    class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full py-2 px-3"
-                    required placeholder="サービス価格" value="{{ old('price', $post->price) }}">
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm mb-2" for="number">
-                    予定作業人数
-                </label>
-                <input type="integer" name="number"
-                    class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full py-2 px-3"
-                    required placeholder="予定作業人数" value="{{ old('number', $post->number) }}">
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm mb-2" for="request">
-                    購入にあたってのお願い
-                </label>
-                <textarea name="request" rows="10"
-                    class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full py-2 px-3"
-                    required placeholder="ユーザーに必ず書いてほしい情報、事業所でできること・できないこと、などを記載してください">{{ old('request', $post->request) }}</textarea>
+                    required placeholder="本文">{{ old('body', $post->body) }}</textarea>
             </div>
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm mb-2" for="image">
